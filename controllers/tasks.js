15a2ea1196dc653d0600bf36ad6f201ae109e4a1
hrom1 <hrom012@gmail.com>
2023-02-22 20:45:02 +0300

    add create task controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,17 @@
+const Task = require('../models/task')
+
 const getAllTasks = (req, res) => {
     res.send('all items');
 };
 
-const createTask = (req, res) => {
-    res.send('create items');
+const createTask = async (req, res) => {
+    try{
+        const task = await Task.create(req.body);
+        res.status(201).json(task);
+    }
+    catch (e){
+        res.status(500).json({message: e.message});
+    }
 };
 
 const getTask = (req, res) => {
